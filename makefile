bdf00d07fabdbf4e2273bf62e3e1d61d0f29b5ee
Tiago de Paula <tiagodepalves@gmail.com>
2021-06-18 16:11:34 -0300

    Semi funcional

--- makefile
+++ makefile
@@ -1,11 +1,11 @@
-CC ?= gcc
-PROG ?= t5
+CC = gcc
+PROG = t5
 
 OPTIM   = -O3 -march=native -mtune=native -pipe -fivopts
 OPTIM  += -fmodulo-sched -flto -fwhole-program -fno-plt
-CFLAGS ?= -std=c99 -Wall -Werror -Wpedantic -Wunused-result $(OPTIM)
+CFLAGS = -std=c99 -Wall -Werror -Wpedantic -Wunused-result $(OPTIM)
 
-VGFLAGS ?= --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --error-exitcode=10
+VGFLAGS = --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --error-exitcode=10
 
 
 .PHONY: all clean debug run
--- t5.c
+++ t5.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #ifdef __GNUC__
 // Atributos do GCC
@@ -81,7 +82,7 @@ static inline
  *
  * Retorna SIZE_MAX em caso de erro;
  */
-size_t custo_total(const grafo_t *rede, unsigned K)
+size_t custo_total(grafo_t *rede, unsigned K)
 attribute(pure, nonnull, hot, nothrow);
 
 int main(void) {
@@ -106,8 +107,120 @@ int main(void) {
  * CUSTO DA REDE *
  * * * * * * * * */
 
+/**
+ *  Representação de conjuntos
+ * disjuntos por matriz booleana.
+ */
+typedef struct conjuntos {
+    size_t tam;              // qtde de conjuntos
+    uint16_t *restrict pos;  // conjunto atual do no 'i'
+    bool *restrict elem;     // matriz marcando elementos
+} conj_t;
+
+
+static inline attribute(nonnull, hot, nothrow)
+/**
+ * Faz a união de dois conjuntos 'a != b'.
+ */
+void uniao(conj_t conj, unsigned a, unsigned b) {
+    // sempre usa 'a' como o menor
+    if unlikely(b < a) {
+        unsigned x = b;
+        b = a; a = x;
+    }
+
+    bool *ca = conj.elem + (a * conj.tam);
+    bool *cb = conj.elem + (b * conj.tam);
+    // para cada elemento de 'b'
+    for (unsigned i = 0; i < conj.tam; i++) {
+        if (cb[i]) {
+            // insere em 'a'
+            ca[i] = true;
+            conj.pos[i] = a;
+        }
+    }
+}
+
+static inline attribute(nonnull, hot, nothrow)
+/**
+ *  Algoritmo de Kruskal modificado que encerra quando
+ * a floresta é reduzida para 'max' árvores.
+ */
+size_t kruskal(const grafo_t *restrict rede, unsigned max, conj_t conj) {
+    unsigned arvores = conj.tam;
+    // checa se o limite já foi alcançado
+    if unlikely(arvores < max) return SIZE_MAX;
+    if unlikely(arvores == max) return 0;
+
+    size_t custo = 0;
+    // assume as arestas já ordernadas
+    for (unsigned e = 0; e < rede->M; e++) {
+        aresta_t a = rede->E[e];
+        // acessa o conjnto de A e B
+        unsigned ca = conj.pos[a.A];
+        unsigned cb = conj.pos[a.B];
+        // e junta se necessário
+        if unlikely(ca != cb) {
+            uniao(conj, ca, cb);
+            custo += a.W;
+            // encerra se alcançar o limite
+            if unlikely(--arvores == max) {
+                return custo;
+            }
+        }
+    }
+    return custo;
+}
+
+static attribute(nonnull, hot, nothrow)
+void quicksort(aresta_t aresta[], unsigned hi) {
+    // particiona o vetor
+    aresta_t pivo = aresta[hi];
+    unsigned i = 0;
+    for (unsigned j = 0; j < hi; j++) {
+        aresta_t cur = aresta[j];
+        if (cur.W < pivo.W) {
+            aresta[j] = aresta[i];
+            aresta[i] = cur;
+            i += 1;
+        }
+    }
+    aresta[hi] = aresta[i];
+    aresta[i] = pivo;
+    // e ordena recursivamente
+    if (i > 1) {
+        quicksort(aresta, i - 1);
+    }
+    if (hi > (i + 1)) {
+        quicksort(aresta + (i+1), hi - (i+1));
+    }
+}
+
 static inline attribute(pure, nonnull, hot, nothrow)
 /* Custo da rede */
-size_t custo_total(const grafo_t *_rede, unsigned _K) {
-    return SIZE_MAX;
+size_t custo_total(grafo_t *rede, unsigned K) {
+    if unlikely(rede->M == 0) {
+        return (K == rede->N)? 0 : SIZE_MAX;
+    }
+    // alocação dos conjuntos
+    uint16_t *pos = malloc(rede->N * sizeof(uint16_t));
+    if unlikely(pos == NULL) return SIZE_MAX;
+    bool *elem = calloc(rede->N * rede->N, sizeof(bool));
+    if unlikely(elem == NULL) {
+        free(pos);
+        return SIZE_MAX;
+    }
+    // prepara o conjunto inicial de cada nó
+    conj_t conj = { .tam = rede->N, .pos = pos, .elem = elem };
+    for (unsigned i = 0; i < rede->N; i++) {
+        pos[i] = i;
+        elem[i * (conj.tam + 1)] = true;
+    }
+    // ordenação das arestas
+    quicksort(rede->E, rede->M - 1);
+    // cálculo por Kruskal
+    size_t custo = kruskal(rede, K, conj);
+    free(pos);
+    free(elem);
+    return custo;
 }
